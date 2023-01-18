e1602d056688af2a16522b19ad051455bc54c39f
= <=>
2023-01-18 22:25:42 +0300

    Update code

---
+++ app/Http/Controllers/Admin/PortfolioController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Portfolio;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Validation\Rule;
+
+class PortfolioController extends Controller
+{
+    public function index(): Factory|View|Application
+    {
+        return view('dashboard.portfolio.index', ['portfolios' => Portfolio::all()]);
+    }
+
+    public function create(): Factory|View|Application
+    {
+        return view('dashboard.portfolio.create');
+    }
+
+    public function edit(Portfolio $portfolio): Factory|View|Application
+    {
+        return view('dashboard.portfolio.edit', ['portfolio' => $portfolio]);
+    }
+
+    public function store(): RedirectResponse
+    {
+        Portfolio::create(array_merge($this->validatePost(), [
+            'thumbnail' => request()->file('thumbnail')->store('portfolios', ['disk' => 'public'])
+        ]));
+        return redirect()->route('portfolio')->with('success', 'Saved!');
+    }
+
+    public function update(Portfolio $portfolio): RedirectResponse
+    {
+        $attributes = $this->validatePost($portfolio);
+
+        if ($attributes['thumbnail'] ?? false) {
+            $attributes['thumbnail'] = request()->file('thumbnail')->store('portfolios', ['disk' => 'public']);
+        }
+
+        $portfolio->update($attributes);
+
+        return back()->with('success', 'Post Updated!');
+    }
+
+    protected function validatePost(?Portfolio $portfolio = null): array
+    {
+        $portfolio ??= new Portfolio();
+        return request()->validate([
+            'title' => 'required',
+            'description' => 'required',
+            'body' => 'required',
+            'slug' => ['required', Rule::unique('portfolios', 'slug')->ignore($portfolio)],
+            'thumbnail' => $portfolio->exists ? ['image', 'max:5096'] : ['required', 'image', 'max:5096']
+        ]);
+    }
+}
--- app/Http/Controllers/PortfolioController.php
+++ app/Http/Controllers/PortfolioController.php
@@ -6,56 +6,16 @@ use App\Models\Portfolio;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
-use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
-use Illuminate\Validation\Rule;
 
-class
-PortfolioController extends Controller
+class PortfolioController extends Controller
 {
     public function index(): Factory|View|Application
     {
-        return view('dashboard.portfolio.index', ['portfolios' => Portfolio::all()]);
+        return view('portfolio', ['portfolios' => Portfolio::query()->select('title', 'slug', 'description', 'thumbnail')->get()]);
     }
 
-    public function create(): Factory|View|Application
+    public function show(Portfolio $portfolio): Factory|View|Application
     {
-        return view('dashboard.portfolio.create');
-    }
-
-    public function edit(Portfolio $portfolio): Factory|View|Application
-    {
-        return view('dashboard.portfolio.edit', ['portfolio' => $portfolio]);
-    }
-
-    public function store(): RedirectResponse
-    {
-        Portfolio::create(array_merge($this->validatePost(), [
-            'thumbnail' => request()->file('thumbnail')->store('portfolios', ['disk' => 'public'])
-        ]));
-        return redirect()->route('portfolio')->with('success', 'Saved!');
-    }
-
-    public function update(Portfolio $portfolio): RedirectResponse
-    {
-        $attributes = $this->validatePost($portfolio);
-
-        $attributes['thumbnail'] = request()->file('thumbnail')->store('portfolios', ['disk' => 'public']);
-
-        $portfolio->update($attributes);
-
-        return back()->with('success', 'Post Updated!');
-    }
-
-    protected function validatePost(?Portfolio $portfolio = null): array
-    {
-        $portfolio ??= new Portfolio();
-        return request()->validate([
-            'title' => 'required',
-            'description' => 'required',
-            'body' => 'required',
-            'slug' => ['required', Rule::unique('portfolios', 'slug')->ignore($portfolio)],
-            'thumbnail' => ['required', 'image', 'max:5096']
-        ]);
+        return view('portfolio.example-one', ['portfolio' => $portfolio]);
     }
 }
