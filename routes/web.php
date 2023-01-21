cf1a145df08079f3325efbc7f45ae4a8726c645a
= <=>
2023-01-21 22:55:00 +0300

    Refactor web routes

--- routes/web.php
+++ routes/web.php
@@ -29,22 +29,24 @@ Route::get('/about', function () {
 
 Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified'])->group(function () {
     Route::prefix('dashboard')->group(function () {
-        Route::get('/', function () {
-            return view('dashboard');
-        })->name('dashboard');
+        Route::view('/', 'dashboard')->name('dashboard');
 
         Route::prefix('portfolio')->group(function () {
-            Route::get('/', [PortfolioController::class, 'index'])->name('admin.portfolio');
-            Route::get('/create', [PortfolioController::class, 'create']);
-            Route::post('/create', [PortfolioController::class, 'store'])->name('admin.portfolio.create');
-            Route::get('/update/{portfolio:id}', [PortfolioController::class, 'edit'])->name('admin.portfolio.edit');
-            Route::post('/update/{portfolio:id}', [PortfolioController::class, 'update'])->name('admin.portfolio.update');
-            Route::delete('/delete/{portfolio:id}', [PortfolioController::class, 'delete'])->name('admin.portfolio.delete');
+            Route::controller(PortfolioController::class)->group(function () {
+                Route::get('/', 'index')->name('admin.portfolio');
+                Route::get('/create', 'create');
+                Route::post('/create', 'store')->name('admin.portfolio.create');
+                Route::get('/update/{portfolio:id}', 'edit')->name('admin.portfolio.edit');
+                Route::post('/update/{portfolio:id}', 'update')->name('admin.portfolio.update');
+                Route::delete('/delete/{portfolio:id}', 'delete')->name('admin.portfolio.delete');
+            });
         });
 
         Route::prefix('archive')->group(function () {
-            Route::get('/', [ArchiveController::class, 'index'])->name('admin.archive');
-            Route::post('/restore', [ArchiveController::class, 'restore'])->name('admin.archive.restore');
+            Route::controller(ArchiveController::class)->group(function () {
+                Route::get('/', 'index')->name('admin.archive');
+                Route::post('/restore', 'restore')->name('admin.archive.restore');
+            });
         });
     });
 });
