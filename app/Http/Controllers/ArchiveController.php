50c3f5e0fe111509cf6ae998b78db7a4325cbba4
kleninmaxim <maksim_klenin_99@mail.ru>
2023-01-24 22:50:36 +0300

    Add validation to Archive

--- app/Http/Controllers/ArchiveController.php
+++ app/Http/Controllers/ArchiveController.php
@@ -6,8 +6,8 @@ use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Validation\Rule;
 
 class ArchiveController extends Controller
 {
@@ -16,10 +16,20 @@ class ArchiveController extends Controller
         return view('dashboard.archive.index', ['archives' => DB::table('deleted_models')->select('id', 'key', 'model', 'values')->orderByDesc('created_at')->get()]);
     }
 
-    public function restore(Request $request): RedirectResponse
+    public function restore(): RedirectResponse
     {
-        $model = new $request['model'];
-        $model::restore($request['key']);
+        $attributes = $this->validateDeletedModel();
+
+        ($attributes['model'])::restore($attributes['key']);
+
         return redirect()->back();
+    }
+
+    protected function validateDeletedModel(): array
+    {
+        return request()->validate([
+            'model' => ['required', 'string', 'max:255', Rule::exists('deleted_models', 'model')],
+            'key' => ['required', 'string', 'max:40', Rule::exists('deleted_models', 'key')]
+        ]);
     }
 }
